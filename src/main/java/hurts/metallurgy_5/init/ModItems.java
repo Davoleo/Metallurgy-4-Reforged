5e3f893e64d00f0503fb7980e8cfb2a6f6daad85
Null <yutuber957@gmail.com>
2018-08-11 20:29:05 +0200

    Second Commit
    added new ingot

--- src/main/java/hurts/metallurgy_5/init/ModItems.java
+++ src/main/java/hurts/metallurgy_5/init/ModItems.java
@@ -10,11 +10,20 @@ public class ModItems {
 	 
 	public static final List <Item> ITEMS = new ArrayList <Item>();
 	
-	public static final Item MAGNESE_INGOT = new ItemBase("magnese_ingot");
+	public static final Item MANGANESE_INGOT = new ItemBase("manganese_ingot");
 	public static final Item HEPATIZON_INGOT = new ItemBase("hepatizon_ingot");
 	public static final Item DAMASCUS_STEAL_INGOT = new ItemBase("damascus_steal_ingot");
 	public static final Item ANGMALLEN_INGOT = new ItemBase("angmallen_ingot");
 	public static final Item MEUTOITE_INGOT = new ItemBase("meutoite_ingot");
+	public static final Item CARMOT_INGOT = new ItemBase("carmot_ingot");
+	public static final Item ASTRAL_SILVER_INGOT = new ItemBase("astral_silver_ingot");
+	public static final Item PROMETHEUM_INGOT = new ItemBase("prometheum_ingot");
+	public static final Item DESICHALKOS_INGOT = new ItemBase("desichalkos_ingot");
+	public static final Item INFUSCOLIUM_INGOT = new ItemBase("infuscolium_ingot");
+	public static final Item EXIMITE_INGOT = new ItemBase("eximite_ingot");
+	public static final Item EPATIZON_INGOT = new ItemBase("hepatizon_ingot");
+	public static final Item BLACK_STEEL_INGOT = new ItemBase("black_steel_ingot");
+	//public static final Item  = new ItemBase("");
 	
-
+	
 }
