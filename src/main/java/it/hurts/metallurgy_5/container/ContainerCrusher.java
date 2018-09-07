e0653ca780569e070b1e0200d5e21d06328bbb25
Davoleo <davoleo2001@gmail.com>
2018-09-07 16:42:54 +0200

    Fixed quick-transfer

--- src/main/java/it/hurts/metallurgy_5/container/ContainerCrusher.java
+++ src/main/java/it/hurts/metallurgy_5/container/ContainerCrusher.java
@@ -91,72 +91,77 @@ public class ContainerCrusher extends Container {
     }
 
 
-    //TODO : Da fixare
     @Override
     public ItemStack transferStackInSlot(EntityPlayer playerIn, int index)
     {
-        ItemStack stack = ItemStack.EMPTY;
+        ItemStack itemstack = ItemStack.EMPTY;
         Slot slot = this.inventorySlots.get(index);
 
-        if(slot != null && slot.getHasStack())
+        if (slot != null && slot.getHasStack())
         {
-            ItemStack stack1 = slot.getStack();
-            stack = stack1.copy();
+            ItemStack itemstack1 = slot.getStack();
+            itemstack = itemstack1.copy();
 
-            if(index == 2)
+            if (index == 2)
             {
-                if(!this.mergeItemStack(stack1, 4, 40, true)) return ItemStack.EMPTY;
-                slot.onSlotChange(stack1, stack);
-            }
-            else if(index != 1 && index != 0)
-            {
-                Slot slot1 = this.inventorySlots.get(index + 1);
-
-                if(!BlockCrusherRecipes.getInstance().getCrushingResult(slot1.getStack()).isEmpty())
+                if (!this.mergeItemStack(itemstack1, 3, 39, true))
                 {
-                    if(!this.mergeItemStack(stack1, 0, 2, false))
-                    {
-                        return ItemStack.EMPTY;
-                    }
-                    else if(TileEntityCrusher.isItemFuel(stack1))
-                    {
-                        if(!this.mergeItemStack(stack1, 2, 3, false)) return ItemStack.EMPTY;
-                    }
-                    else if(TileEntityCrusher.isItemFuel(stack1))
-                    {
-                        if(!this.mergeItemStack(stack1, 2, 3, false)) return ItemStack.EMPTY;
-                    }
-                    else if(TileEntityCrusher.isItemFuel(stack1))
-                    {
-                        if(!this.mergeItemStack(stack1, 2, 3, false)) return ItemStack.EMPTY;
-                    }
-                    else if(index >= 4 && index < 31)
-                    {
-                        if(!this.mergeItemStack(stack1, 31, 40, false)) return ItemStack.EMPTY;
-                    }
-                    else if(index >= 31 && index < 40 && !this.mergeItemStack(stack1, 4, 31, false))
+                    return ItemStack.EMPTY;
+                }
+
+                slot.onSlotChange(itemstack1, itemstack);
+            }
+            else if (index != 1 && index != 0)
+            {
+                if (!BlockCrusherRecipes.getInstance().getCrushingResult(itemstack1).isEmpty())
+                {
+                    if (!this.mergeItemStack(itemstack1, 0, 1, false))
                     {
                         return ItemStack.EMPTY;
                     }
                 }
+                else if (TileEntityCrusher.isItemFuel(itemstack1))
+                {
+                    if (!this.mergeItemStack(itemstack1, 1, 2, false))
+                    {
+                        return ItemStack.EMPTY;
+                    }
+                }
+                else if (index >= 3 && index < 30)
+                {
+                    if (!this.mergeItemStack(itemstack1, 30, 39, false))
+                    {
+                        return ItemStack.EMPTY;
+                    }
+                }
+                else if (index >= 30 && index < 39 && !this.mergeItemStack(itemstack1, 3, 30, false))
+                {
+                    return ItemStack.EMPTY;
+                }
             }
-            else if(!this.mergeItemStack(stack1, 4, 40, false))
+            else if (!this.mergeItemStack(itemstack1, 3, 39, false))
             {
                 return ItemStack.EMPTY;
             }
-            if(stack1.isEmpty())
+
+            if (itemstack1.isEmpty())
             {
                 slot.putStack(ItemStack.EMPTY);
             }
             else
             {
                 slot.onSlotChanged();
-
             }
-            if(stack1.getCount() == stack.getCount()) return ItemStack.EMPTY;
-            slot.onTake(playerIn, stack1);
+
+            if (itemstack1.getCount() == itemstack.getCount())
+            {
+                return ItemStack.EMPTY;
+            }
+
+            slot.onTake(playerIn, itemstack1);
         }
-        return stack;
+
+        return itemstack;
     }
 
 }
