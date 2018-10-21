bdf2ee5fc8d8c1805c3d5f54717f108ea0f6f24f
Null <yutuber957@gmail.com>
2018-10-21 17:17:16 +0200

    Added new Effect to sword

--- src/main/java/it/hurts/metallurgy_5/item/armor/ItemArmorBase.java
+++ src/main/java/it/hurts/metallurgy_5/item/armor/ItemArmorBase.java
@@ -87,31 +87,6 @@ public class ItemArmorBase extends net.minecraft.item.ItemArmor{
     @Override
 	public void onArmorTick(World world, EntityPlayer player, ItemStack item) {
 		
-		if (player.inventory.armorItemInSlot(3).getItem() == ModArmors.astral_silver_helmet
-			&&player.inventory.armorItemInSlot(2).getItem() == ModArmors.astral_silver_chest
-			&&player.inventory.armorItemInSlot(1).getItem() == ModArmors.astral_silver_legs
-			&&player.inventory.armorItemInSlot(0).getItem() == ModArmors.astral_silver_boots){
-			player.addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 20, 2));
-		}else {
-			player.removeActivePotionEffect(MobEffects.JUMP_BOOST);
-		}
-		
-		if (player.inventory.armorItemInSlot(3).getItem() == ModArmors.prometheum_helmet
-			&&player.inventory.armorItemInSlot(2).getItem() == ModArmors.prometheum_chest
-			&&player.inventory.armorItemInSlot(1).getItem() == ModArmors.prometheum_legs
-			&&player.inventory.armorItemInSlot(0).getItem() == ModArmors.prometheum_boots){
-			player.removePotionEffect(MobEffects.POISON);
-		}
-		
-		if (player.inventory.armorItemInSlot(3).getItem() == ModArmors.carmot_helmet
-			&&player.inventory.armorItemInSlot(2).getItem() == ModArmors.carmot_chest
-			&&player.inventory.armorItemInSlot(1).getItem() == ModArmors.carmot_legs
-			&&player.inventory.armorItemInSlot(0).getItem() == ModArmors.carmot_boots) {
-			player.addPotionEffect(new PotionEffect(MobEffects.HASTE, 20, 1));
-		}else {
-			player.removePotionEffect(MobEffects.HASTE);
-		}
-		
 		if (player.inventory.armorItemInSlot(3).getItem() == ModArmors.adamantine_helmet
 				&&player.inventory.armorItemInSlot(2).getItem() == ModArmors.adamantine_chest
 				&&player.inventory.armorItemInSlot(1).getItem() == ModArmors.adamantine_legs
@@ -121,17 +96,5 @@ public class ItemArmorBase extends net.minecraft.item.ItemArmor{
 				player.removePotionEffect(MobEffects.SATURATION);
 			}
 	}
-
-//	@SideOnly(Side.CLIENT)
-//    public static String getPotionDurationString(PotionEffect effect, float durationFactor)
-//    {
-//        if (effect.getDuration()==20)
-//        {
-//            return "**:**";
-//        }else {
-//        	return "**:**";
-//        }
-//		
-//    }
 	
 }
