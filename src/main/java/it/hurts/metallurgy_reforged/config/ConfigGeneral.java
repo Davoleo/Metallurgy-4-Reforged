9780597a7c0ef80b627bb875aa402662b09a01a2
Davoleo <davoleo2001@gmail.com>
2018-12-06 22:35:06 +0100

    Mappings piss me off :confused: | General Config

---
+++ src/main/java/it/hurts/metallurgy_reforged/config/ConfigGeneral.java
@@ -0,0 +1,20 @@
+package it.hurts.metallurgy_reforged.config;
+
+import it.hurts.metallurgy_reforged.Metallurgy;
+import net.minecraftforge.common.config.Config;
+
+/*************************************************
+ * Author: Davoleo
+ * Date / Hour: 06/12/2018 / 21:30
+ * Class: ConfigGeneral
+ * Project: Metallurgy 4 Reforged
+ * Copyright - © - Davoleo - 2018
+ **************************************************/
+
+@Config(modid = Metallurgy.MODID, name = "General Configuration")
+public class ConfigGeneral {
+
+    public static int test = 100;
+
+
+}
