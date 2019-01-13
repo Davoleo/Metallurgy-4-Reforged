c28522ef95350d03989b8c85a6d2b5ae973f065a
Davoleo <davoleo2001@gmail.com>
2019-01-13 23:57:16 +0100

    #86 Fixed OP Swords | Close #86

--- src/main/java/it/hurts/metallurgy_reforged/material/ModMetals.java
+++ src/main/java/it/hurts/metallurgy_reforged/material/ModMetals.java
@@ -23,75 +23,76 @@ public class ModMetals {
     private static final float UNBREAKABLE_TIER_BLAST_RESISTANCE = 18000000F;           //Bedrock-like
     
 //    MetalStats("name", "Name", harvestLevelOfMetalBlock, blastResistance, ArmorStats, ToolStats, FluidStats, harvestLevelOfOreBlock).createMetal;
+    //damage_parameter = in-game_sword_damage - 3
 
     public static final Metal ADAMANTINE = new MetalStats("adamantine", "Adamantine", 2, HIGH_TIER_BLAST_RESISTANCE,
             new ArmorStats(new int[]{3, 4, 5, 3}, 8, 36, 3.5F, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND),
-            new ToolStats(22, 7, 1550, 10F, 8F), new FluidStats(0xFFa30000), 6).createMetal();
+            new ToolStats(22, 7, 1550, 10F, 5F), new FluidStats(0xFFa30000), 6).createMetal();
     public static final Metal ALDUORITE = new MetalStats("alduorite", "Alduorite", 2, MID_TIER_BLAST_RESISTANCE, null, null, new FluidStats(0xFF29AF9D), 3).createMetal();
     public static final Metal ANGMALLEN = new MetalStats("angmallen", "Angmallen", 2, MID_TIER_BLAST_RESISTANCE,
             new ArmorStats(new int[]{3, 5, 6, 3}, 18, 30, 0F, SoundEvents.ITEM_ARMOR_EQUIP_IRON),
-            new ToolStats(30, 3, 300, 7F, 6F), new FluidStats(0xFFF1E472), -1).createMetal();
+            new ToolStats(30, 3, 300, 7F, 3F), new FluidStats(0xFFF1E472), -1).createMetal();
     public static final Metal ASTRAL_SILVER = new MetalStats("astral_silver", "AstralSilver", 2, MID_TIER_BLAST_RESISTANCE,
             new ArmorStats(new int[]{2, 5, 6, 2}, 9, 15, 0.0F, SoundEvents.ITEM_ARMOR_EQUIP_GENERIC),
-            new ToolStats(30, 5, 350, 12F, 5F), new FluidStats(0xFF70C4C4), 4).createMetal();
+            new ToolStats(30, 5, 350, 12F, 2F), new FluidStats(0xFF70C4C4), 4).createMetal();
     public static final Metal ATLARUS = new MetalStats("atlarus", "Atlarus", 2, HIGH_TIER_BLAST_RESISTANCE,
             new ArmorStats(new int[]{4, 3, 3, 4}, 2, 35, 3.5F, SoundEvents.ITEM_ARMOR_EQUIP_IRON),
-            new ToolStats(22, 7, 1750, 10F, 8F), new FluidStats(0xFFC0C601), 6).createMetal();
+            new ToolStats(22, 7, 1750, 10F, 5F), new FluidStats(0xFFC0C601), 6).createMetal();
     public static final Metal AMORDRINE = new MetalStats("amordrine", "Amordrine", 2, MID_TIER_BLAST_RESISTANCE,
             new ArmorStats(new int[]{3, 4, 5, 3}, 50, 50, 2F, SoundEvents.ITEM_ARMOR_EQUIP_IRON),
-            new ToolStats(50, 5, 500, 14F, 7F), new FluidStats(0xFFB981F1), -1).createMetal();
+            new ToolStats(50, 5, 500, 14F, 4F), new FluidStats(0xFFB981F1), -1).createMetal();
     public static final Metal BLACK_STEEL = new MetalStats("black_steel", "BlackSteel", 2, HIGH_TIER_BLAST_RESISTANCE,
             new ArmorStats(new int[]{3, 5, 6, 3}, 17, 50, 4F, SoundEvents.ITEM_ARMOR_EQUIP_IRON),
-            new ToolStats(17, 3, 500, 8F, 6F), new FluidStats(0xFF010623), -1).createMetal();
+            new ToolStats(17, 3, 500, 8F, 3F), new FluidStats(0xFF010623), -1).createMetal();
     public static final Metal BRASS = new MetalStats("brass", "Brass", 2, MID_TIER_BLAST_RESISTANCE,
             new ArmorStats(new int[]{1, 2, 3, 2}, 18, 15, 4F, SoundEvents.ITEM_ARMOR_EQUIP_IRON),
-            new ToolStats(18, 1, 15, 10F, 5F), new FluidStats(0xFFEAAB00), -1).createMetal();
+            new ToolStats(18, 1, 15, 10F, 2F), new FluidStats(0xFFEAAB00), -1).createMetal();
     public static final Metal BRONZE = new MetalStats("bronze", "Bronze", 2, HIGH_TIER_BLAST_RESISTANCE,
             new ArmorStats(new int[]{3, 3, 4, 2}, 9, 25, 2F, SoundEvents.ITEM_ARMOR_EQUIP_IRON),
-            new ToolStats(9, 3, 250, 6F, 5F), new FluidStats(0xFFFFB301), -1).createMetal();
+            new ToolStats(9, 3, 250, 6F, 2F), new FluidStats(0xFFFFB301), -1).createMetal();
     public static final Metal CARMOT = new MetalStats("carmot", "Carmot", 2, HIGH_TIER_BLAST_RESISTANCE,
             new ArmorStats(new int[]{2, 4, 5, 2}, 7, 28, 0.0F, SoundEvents.ITEM_ARMOR_EQUIP_IRON),
-            new ToolStats(40, 5, 500, 12F, 5F), new FluidStats(0xFFE5D868), 4).createMetal();
+            new ToolStats(40, 5, 500, 12F, 2F), new FluidStats(0xFFE5D868), 4).createMetal();
     public static final Metal CELENEGIL = new MetalStats("celenegil", "Celenegil", 2, EXTREME_TIER_BLAST_RESISTANCE,
             new ArmorStats(new int[]{4, 6, 7, 5}, 50, 160, 0F, SoundEvents.ITEM_ARMOR_EQUIP_IRON),
-            new ToolStats(50, 6, 1600, 14F, 7F), new FluidStats(0xFF86E51A), -1).createMetal();
+            new ToolStats(50, 6, 1600, 14F, 4F), new FluidStats(0xFF86E51A), -1).createMetal();
     public static final Metal CERUCLASE = new MetalStats("ceruclase", "Ceruclase", 2, HIGH_TIER_BLAST_RESISTANCE,
     		new ArmorStats(new int[]{3, 6, 5, 4}, 50, 137, 0F, SoundEvents.ITEM_ARMOR_EQUIP_IRON),
-            new ToolStats(18, 4, 500, 7, 7F), new FluidStats(0xFF4CA8E5), 3).createMetal();
+            new ToolStats(18, 4, 500, 7, 4F), new FluidStats(0xFF4CA8E5), 3).createMetal();
     public static final Metal COPPER = new MetalStats("copper", "Copper", 2, LOW_TIER_BLAST_RESISTANCE,
     		new ArmorStats(new int[]{2, 3, 2, 1}, 50, 5, 0F, SoundEvents.ITEM_ARMOR_EQUIP_IRON),
-            new ToolStats(5, 2, 180, 5F, 5F), new FluidStats(0xFFE5770A), 1).createMetal();
+            new ToolStats(5, 2, 180, 5F, 2F), new FluidStats(0xFFE5770A), 1).createMetal();
     public static final Metal DAMASCUS_STEEL = new MetalStats("damascus_steel", "DamascusSteel", 2, UNBREAKABLE_TIER_BLAST_RESISTANCE,
             new ArmorStats(new int[]{3, 5, 6, 3}, 18, 50, 3F, SoundEvents.ITEM_ARMOR_EQUIP_IRON),
-            new ToolStats(18, 4, 500, 6F, 6F), new FluidStats(0xFF75663D), -1).createMetal();
+            new ToolStats(18, 4, 500, 6F, 3F), new FluidStats(0xFF75663D), -1).createMetal();
     public static final Metal DEEP_IRON = new MetalStats("deep_iron", "DeepIron", 2, MID_TIER_BLAST_RESISTANCE,
             new ArmorStats(new int[]{2, 4, 5, 2}, 1, 38, 0.0F, SoundEvents.ITEM_ARMOR_EQUIP_IRON),
-            new ToolStats(14, 3, 250, 6F, 6F), new FluidStats(0xFF383C72), 2).createMetal();
+            new ToolStats(14, 3, 250, 6F, 3F), new FluidStats(0xFF383C72), 2).createMetal();
     public static final Metal DESICHALKOS = new MetalStats("desichalkos", "Desichalkos", 2, HIGH_TIER_BLAST_RESISTANCE,
             new ArmorStats(new int[]{4, 5, 7, 4}, 30, 180, 3F, SoundEvents.ITEM_ARMOR_EQUIP_IRON),
-            new ToolStats(30, 4, 1800, 8F, 8F), new FluidStats(0xFF420098), -1).createMetal();
+            new ToolStats(30, 4, 1800, 8F, 5F), new FluidStats(0xFF420098), -1).createMetal();
     public static final Metal ELECTRUM = new MetalStats("electrum", "Electrum", 2, LOW_TIER_BLAST_RESISTANCE,
             new ArmorStats(new int[]{3, 5, 6, 2}, 30, 51, 3F, SoundEvents.ENTITY_LIGHTNING_IMPACT),
-            new ToolStats(30, 2, 70, 14F, 5F), new FluidStats(0xFFEFEF57), -1).createMetal();
+            new ToolStats(30, 2, 70, 14F, 2F), new FluidStats(0xFFEFEF57), -1).createMetal();
     public static final Metal EXIMITE = new MetalStats("eximite", "Eximite", 2, HIGH_TIER_BLAST_RESISTANCE,
             new ArmorStats(new int[]{4, 5, 6, 4}, 25, 100, 5.0F, SoundEvents.ENTITY_ENDERDRAGON_GROWL),
-            new ToolStats(25, 7, 1000, 8F, 7F), new FluidStats(0xFF5E4191), 3).createMetal();
+            new ToolStats(25, 7, 1000, 8F, 4F), new FluidStats(0xFF5E4191), 3).createMetal();
     public static final Metal HADEROTH = new MetalStats("haderoth", "Haderoth", 2, EXTREME_TIER_BLAST_RESISTANCE,
             new ArmorStats(new int[]{4, 5, 7, 4}, 19, 125, 6F, SoundEvents.ITEM_ARMOR_EQUIP_IRON),
-            new ToolStats(19, 5, 1250, 12F, 7F), new FluidStats(0xFF963700), -1).createMetal();
+            new ToolStats(19, 5, 1250, 12F, 4F), new FluidStats(0xFF963700), -1).createMetal();
     public static final Metal HEPATIZON = new MetalStats("hepatizon", "Hepatizon", 2, HIGH_TIER_BLAST_RESISTANCE,
             new ArmorStats(new int[]{3, 3, 4, 2}, 22, 57, 2F, SoundEvents.ITEM_ARMOR_EQUIP_IRON),
-            new ToolStats(22, 3, 300, 8F, 5F), new FluidStats(0xFF755E86), -1).createMetal();
+            new ToolStats(22, 3, 300, 8F, 2F), new FluidStats(0xFF755E86), -1).createMetal();
     public static final Metal IGNATIUS = new MetalStats("ignatius", "Ignatius", 2, LOW_TIER_BLAST_RESISTANCE,
             new ArmorStats(new int[]{2, 5, 6, 2}, 15, 24, 0.0F, SoundEvents.ITEM_ARMOR_EQUIP_IRON),
-            new ToolStats(15, 2, 200, 4F, 6F), new FluidStats(0xFFFF9100), 1).createMetal();
+            new ToolStats(15, 2, 200, 4F, 3F), new FluidStats(0xFFFF9100), 1).createMetal();
     public static final Metal INFUSCOLIUM = new MetalStats("infuscolium", "Infuscolium", 2, MID_TIER_BLAST_RESISTANCE, null, null, new FluidStats(0xFF7F036E), 2).createMetal();
     public static final Metal INOLASHITE = new MetalStats("inolashite", "Inolashite", 2, EXTREME_TIER_BLAST_RESISTANCE,
             new ArmorStats(new int[]{3, 5, 7, 4}, 25, 70, 4F, SoundEvents.ITEM_ARMOR_EQUIP_IRON),
-            new ToolStats(25, 5, 900, 8F, 7F), new FluidStats(0xFF00A579), -1).createMetal();
+            new ToolStats(25, 5, 900, 8F, 4F), new FluidStats(0xFF00A579), -1).createMetal();
     public static final Metal KALENDRITE = new MetalStats("kalendrite", "Kalendrite", 2, HIGH_TIER_BLAST_RESISTANCE,
             new ArmorStats(new int[]{4, 5, 6, 4}, 20, 40, 3.0F, SoundEvents.ITEM_ARMOR_EQUIP_IRON),
-            new ToolStats(20, 5, 1000, 8F, 7F), new FluidStats(0xFF9941DD), 4).createMetal();
+            new ToolStats(20, 5, 1000, 8F, 4F), new FluidStats(0xFF9941DD), 4).createMetal();
     public static final Metal LEMURITE = new MetalStats("lemurite", "Lemurite", 2, LOW_TIER_BLAST_RESISTANCE, null, null, new FluidStats(0xFFD0D0D0), 2).createMetal();
     public static final Metal MANGANESE = new MetalStats("manganese", "Manganese", 2, MID_TIER_BLAST_RESISTANCE, null, null, new FluidStats(0xFFFFAEDF), 3).createMetal();
     public static final Metal MEUTOITE = new MetalStats("meutoite", "Meutoite", 2, HIGH_TIER_BLAST_RESISTANCE, null, null, new FluidStats(0xFF533468), 4).createMetal();
