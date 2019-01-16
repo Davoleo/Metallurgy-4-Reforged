2c6a9f1b99a20037bd5a37d8a467cbd9ab0e7212
Davoleo <davoleo2001@gmail.com>
2019-01-16 20:04:14 +0100

    Alpha warning message is now translatable

--- src/main/java/it/hurts/metallurgy_reforged/util/OnPlayerJoin.java
+++ src/main/java/it/hurts/metallurgy_reforged/util/OnPlayerJoin.java
@@ -27,11 +27,11 @@ public class OnPlayerJoin {
 		GITHUB_REPO.getStyle().setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, GITHUB_REPO.getText().substring(2, 50)));
 
 		if(!event.getEntity().world.isRemote) {
-			player.sendMessage(new TextComponentString("\u00A74\u00A7lMetallurgy 4: Reforged \u00A7r\n\u00A76[Warning: this is an alpha version of the mod]" +
-                    "\nFurther integration with common mods (like Tinkers' Construct) is not currently available \u00A7lbut planned."));
-			player.sendMessage(new TextComponentString("\nIf you want more info/take a look at the machine recipes - go to the github repo wiki:"));
+			player.sendMessage(new TextComponentString(Utils.localize("util.world_join_message.1")));
+			player.sendMessage(new TextComponentString(Utils.localize("util.world_join_message.2")));
+			player.sendMessage(new TextComponentString(Utils.localize("util.world_join_message.3")));
+			player.sendMessage(new TextComponentString(Utils.localize("util.world_join_message.4")));
 			player.sendMessage(GITHUB_REPO);
-			player.sendMessage(new TextComponentString("Bug reports go on the github repo aswell"));
 
 		}
 	}
