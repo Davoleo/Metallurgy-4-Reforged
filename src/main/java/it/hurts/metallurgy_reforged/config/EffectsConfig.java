90fd74322d0b46d01b0aabbc5171bf5e2be54616
Davoleo <davoleo2001@gmail.com>
2019-01-25 23:29:46 +0100

    Config Comments

--- src/main/java/it/hurts/metallurgy_reforged/config/EffectsConfig.java
+++ src/main/java/it/hurts/metallurgy_reforged/config/EffectsConfig.java
@@ -22,35 +22,59 @@ public class EffectsConfig {
     @Config.Comment("Saturation Effect")
     @Config.Ignore
     public static boolean adamantineArmorEffect = true;
+    @Config.Comment("Strength II")
     public static boolean amordrineArmorEffect = true;
+    @Config.Comment("Luck")
     public static boolean angmallenArmorEffect = true;
+    @Config.Comment("Jump Boost")
     public static boolean astralSilverArmorEffect = true;
+    @Config.Comment("Haste")
     public static boolean carmotArmorEffect = true;
+    @Config.Comment("Resistance")
     public static boolean celenegilArmorEffect = true;
+    @Config.Comment("Fast underwater movements")
     public static boolean deepIronArmorEffect = true;
+    @Config.Comment("The helmet makes endermen stay neutral when you watch them")
     public static boolean eximiteArmorEffect = true;
+    @Config.Comment("Strength I")
     public static boolean kaledriteArmorEffect = true;
+    @Config.Comment("Makes Entities around you glow")
     public static boolean mithrilArmorEffect = true;
+    @Config.Comment("The helmet gives you night vision")
     public static boolean platinumArmorEffect = true;
+    @Config.Comment("Removes poison effect from yourself")
     public static boolean prometheumArmorEffect = true;
+    @Config.Comment("Speeds you up")
     public static boolean quicksilverArmorEffect = true;
+    @Config.Comment("Absorbed damage is proportional to the darkness")
     public static boolean shadowSteelArmorEffect = true;
+    @Config.Comment("You can't take damage from fire")
     public static boolean vulcaniteArmorEffect = true;
 
     //Pickaxes
+    @Config.Comment("Underwater mining is not slowed down")
     public static boolean deepIronPickaxeEffect = true;
 
-    //Tools 
+    //Tools
+    @Config.Comment("The tool speed is proportional to the darkness")
     public static boolean shadowSteelToolSpeedEffect = true;
 
     //Swords
+    @Config.Comment("Gives some random effect to the target")
     public static boolean desichalkosSwordEffect = true;
+    @Config.Comment("Fire aspect")
     public static boolean ignatiusSwordEffect = true;
+    @Config.Comment("Chance to regenerate your life on hit")
     public static boolean kalendriteSwordEffect = true;
+    @Config.Comment("Chance to blind the target")
     public static boolean shadowIronSwordEffect = true;
+    @Config.Comment("Speed and Damage is proportional to the darkness")
     public static boolean shadowSteelSwordEffect = true;
+    @Config.Comment("Wither")
     public static boolean tartariteSwordEffect = true;
+    @Config.Comment("Fire Aspect")
     public static boolean vulcaniteSwordEffect = true;
+    @Config.Comment("Poison")
     public static boolean vyroxeresSwordEffect = true;
 
     private EffectsConfig(){}
