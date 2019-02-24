8881878d80c0dbed561cf4d7c3608d720e21c025
HurtsAsHell <yutuber957@gmail.com>
2019-02-24 03:44:38 +0100

    Added Secondary Block From Bitumen Ore

--- src/main/java/it/hurts/metallurgy_reforged/block/BlockOreDict.java
+++ src/main/java/it/hurts/metallurgy_reforged/block/BlockOreDict.java
@@ -4,11 +4,15 @@ import it.hurts.metallurgy_reforged.material.IOreDict;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.world.Explosion;
 import net.minecraftforge.oredict.OreDictionary;
 
 import javax.annotation.Nonnull;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /***************************
@@ -23,7 +27,8 @@ import java.util.Random;
 public class BlockOreDict extends BlockBase implements IOreDict {
 
 	private String oreName;
-	private Item customDrop;
+	private List<Item> customDrop = new ArrayList<Item>();
+	private Item itemDrop;
 
 	public BlockOreDict(String name, String oreName) {
 		this(name, oreName, null,"p", 1, 5F);
@@ -38,7 +43,17 @@ public class BlockOreDict extends BlockBase implements IOreDict {
 	public BlockOreDict(String name, String oreName, Item drop, String toolClass, int harvestLevel, float blastResistance){
 		super(Material.ROCK, name);
 		this.oreName = oreName;
-		this.customDrop = drop;
+		this.customDrop.add(drop);
+		setHardness(3f);
+		setResistance(blastResistance);
+		this.setHarvestLevel(toolClass, harvestLevel);
+	}
+	
+	public BlockOreDict(String name, String oreName, String toolClass, int harvestLevel, float blastResistance, Item...drop){
+		super(Material.ROCK, name);
+		this.oreName = oreName;
+		for(Item i : drop)
+			customDrop.add(i);
 		setHardness(3f);
 		setResistance(blastResistance);
 		this.setHarvestLevel(toolClass, harvestLevel);
@@ -63,12 +78,34 @@ public class BlockOreDict extends BlockBase implements IOreDict {
     public Item getItemDropped(IBlockState state, Random rand, int fortune){
 		
 //		Check if the drop is an item, else return the same block
-        if(customDrop != null)
-        	return customDrop;
+        if(!customDrop.isEmpty())
+        	if(customDrop.size() == 1) {
+        		if(itemDrop != null)
+        			customDrop.add(itemDrop);
+        		return customDrop.get(0);
+        	} else {
+        		if(canDrop(customDrop.get(0), 25, "bitumen")) {
+        			itemDrop = customDrop.get(0);
+            		customDrop.remove(0);
+            		getItemDropped(state, rand, fortune);
+            		return itemDrop;
+        		}else
+        			return customDrop.get(1);
+        	}
         else
         	return Item.getItemFromBlock(this);
     }
 
+	private boolean canDrop(Item item, int percentance, String name) {
+		if(item.getRegistryName().toString().equals("metallurgy:" + name))
+			if((int) (Math.random() * 100) < percentance)
+				return true;
+			else
+				return false;
+		else
+			return true;
+	}
+	
     @Override
     public int quantityDropped(Random random)
     {
