72444c35dcb7571053f0a4b90f31b2b29993364d
Davoleo <davoleo2001@gmail.com>
2019-03-04 16:05:30 +0100

    Fixed broken copyright

--- src/main/java/it/hurts/metallurgy_reforged/integration/mods/IntegrationCArmory.java
+++ src/main/java/it/hurts/metallurgy_reforged/integration/mods/IntegrationCArmory.java
@@ -1,6 +1,14 @@
-/**
- * 
+/*
+ * -------------------------------------------------------------------------------------------------------
+ * Class: IntegrationCArmory
+ * This class is part of Metallurgy 4 Reforged
+ * Complete source code is available at: https://github.com/Davoleo/Metallurgy-4-Reforged
+ * This code is licensed under GNU GPLv3
+ * Authors: ItHurtsLikeHell & Davoleo
+ * Copyright (c) 2019.
+ * --------------------------------------------------------------------------------------------------------
  */
+
 package it.hurts.metallurgy_reforged.integration.mods;
 
 import c4.conarm.lib.materials.CoreMaterialStats;
@@ -13,14 +21,6 @@ import it.hurts.metallurgy_reforged.material.ModMetals;
 import slimeknights.tconstruct.library.TinkerRegistry;
 import slimeknights.tconstruct.library.materials.Material;
 
-/***************************
- *
- * Author : ItHurtsLikeHell
- * Project: Metallurgy-4-Reforged
- * Date   : 3 mar 2019
- * Time   : 19:39:05
- *
- ****************************/
 public class IntegrationCArmory {
 
 	public static void preInit() {
--- src/main/java/it/hurts/metallurgy_reforged/integration/mods/conarm/MetallurgyConArmorStats.java
+++ src/main/java/it/hurts/metallurgy_reforged/integration/mods/conarm/MetallurgyConArmorStats.java
@@ -1,6 +1,14 @@
-/**
- * 
+/*
+ * -------------------------------------------------------------------------------------------------------
+ * Class: MetallurgyConArmorStats
+ * This class is part of Metallurgy 4 Reforged
+ * Complete source code is available at: https://github.com/Davoleo/Metallurgy-4-Reforged
+ * This code is licensed under GNU GPLv3
+ * Authors: ItHurtsLikeHell & Davoleo
+ * Copyright (c) 2019.
+ * --------------------------------------------------------------------------------------------------------
  */
+
 package it.hurts.metallurgy_reforged.integration.mods.conarm;
 
 import c4.conarm.lib.materials.ArmorMaterials;
@@ -9,14 +17,6 @@ import c4.conarm.lib.materials.PlatesMaterialStats;
 import c4.conarm.lib.materials.TrimMaterialStats;
 import it.hurts.metallurgy_reforged.material.Metal;
 
-/***************************
- *
- * Author : ItHurtsLikeHell
- * Project: Metallurgy-4-Reforged
- * Date   : 3 mar 2019
- * Time   : 18:27:52
- *
- ****************************/
 public class MetallurgyConArmorStats extends ArmorMaterials{
 	
 	public static TrimMaterialStats getTrimStats(Metal metal) {
