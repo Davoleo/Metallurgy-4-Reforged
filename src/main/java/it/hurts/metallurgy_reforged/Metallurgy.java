c415833648abc2eca1bb416058c205f9068c7765
Davoleo <davoleo2001@gmail.com>
2019-06-20 10:08:33 +0200

    Bump version to 0.2.1 + adjust crusher name color color p2

--- src/main/java/it/hurts/metallurgy_reforged/Metallurgy.java
+++ src/main/java/it/hurts/metallurgy_reforged/Metallurgy.java
@@ -50,7 +50,7 @@ public class Metallurgy {
 
 	public static final String MODID = "metallurgy";
 	public static final String NAME = "Metallurgy 4: Reforged";
-	public static final String VERSION = "0.2.0";
+	public static final String VERSION = "0.2.1";
 
 	public static Logger logger;
 
--- src/main/java/it/hurts/metallurgy_reforged/gui/GuiCrusher.java
+++ src/main/java/it/hurts/metallurgy_reforged/gui/GuiCrusher.java
@@ -45,7 +45,7 @@ public class GuiCrusher extends GuiContainer {
     protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY)
     {
         String name = this.crusher.getDisplayName().getUnformattedText();
-        this.fontRenderer.drawString(name, 10, -22, 16747293);
+        this.fontRenderer.drawString(name, 10, -22, 0xFE3F3F);
     }
 
     @Override
