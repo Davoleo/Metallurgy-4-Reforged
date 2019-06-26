cf0ead951e15200ea56808020e7f74f30f5a0acd
HurtsAsHell <yutuber957@gmail.com>
2019-06-26 13:05:24 +0200

    Crossbow and Bow Tinker stats

--- src/main/java/it/hurts/metallurgy_reforged/integration/mods/tic/material/TiCMaterials.java
+++ src/main/java/it/hurts/metallurgy_reforged/integration/mods/tic/material/TiCMaterials.java
@@ -37,7 +37,10 @@ public class TiCMaterials {
 			ToolStats TStats = metalS.getToolStats();
 			if(TStats != null)
 			{
-				registerStats(material, MetallurgyTiCStats.getHeadA(metal),MetallurgyTiCStats.getHandleA(metal),MetallurgyTiCStats.getExtraA(metal));
+				registerStats(material, MetallurgyTiCStats.getHeadA(metal),
+						MetallurgyTiCStats.getHandleA(metal),
+						MetallurgyTiCStats.getExtraA(metal),
+						MetallurgyTiCStats.getBowA(metal));
 			}
 		}	
 	}
