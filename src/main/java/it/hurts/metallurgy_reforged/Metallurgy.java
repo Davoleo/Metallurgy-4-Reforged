a97351021467bd8a3fc43f7d9ba9fce7b3eb4535
Davoleo <davoleo2001@gmail.com>
2019-07-04 22:04:45 +0200

    Bump version to 0.2.2

--- src/main/java/it/hurts/metallurgy_reforged/Metallurgy.java
+++ src/main/java/it/hurts/metallurgy_reforged/Metallurgy.java
@@ -49,7 +49,7 @@ public class Metallurgy {
 
 	public static final String MODID = "metallurgy";
 	public static final String NAME = "Metallurgy 4: Reforged";
-	public static final String VERSION = "0.2.1";
+	public static final String VERSION = "0.2.2";
 
 	public static Logger logger;
 
