b5f8b0873a3e6ed46dae4e05eb013ed1e1ba569c
Davoleo <davoleo2001@gmail.com>
2020-02-06 19:48:18 +0100

    Bump Version to 0.3.1

--- src/main/java/it/hurts/metallurgy_reforged/Metallurgy.java
+++ src/main/java/it/hurts/metallurgy_reforged/Metallurgy.java
@@ -52,7 +52,7 @@
 
 	 public static final String MODID = "metallurgy";
 	 public static final String NAME = "Metallurgy 4: Reforged";
-	 public static final String VERSION = "0.2.3";
+	 public static final String VERSION = "0.3.1";
 
 	 public static Logger logger;
 
