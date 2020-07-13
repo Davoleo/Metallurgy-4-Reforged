028e22ae380694560a76c1c09b51d5608f2039fc
Davoleo <davoleo2001@gmail.com>
2020-07-13 13:17:04 +0200

    Animated ore Particles + Fix #251

--- src/main/java/it/hurts/metallurgy_reforged/particle/ParticleOre.java
+++ src/main/java/it/hurts/metallurgy_reforged/particle/ParticleOre.java
@@ -11,20 +11,36 @@
 
 package it.hurts.metallurgy_reforged.particle;
 
-import it.hurts.metallurgy_reforged.handler.RegistrationHandler;
+import it.hurts.metallurgy_reforged.Metallurgy;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.particle.ParticleRedstone;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 
 public class ParticleOre extends ParticleRedstone {
 
-	public ParticleOre(World worldIn, double xCoordIn, double yCoordIn, double zCoordIn, float scale, float red, float green, float blue, int type)
+	private static final ResourceLocation[] TEXTURES = new ResourceLocation[10];
+
+	static
+	{
+		for (int i = 0; i < 10; i++)
+			TEXTURES[i] = new ResourceLocation(Metallurgy.MODID, "particles/ore_particle_" + (i + 1));
+	}
+
+	private int textureIndex = 0;
+	private int prevTextureIndex = 0;
+	private int level;
+
+	public ParticleOre(World worldIn, double xCoordIn, double yCoordIn, double zCoordIn, float scale, float red, float green, float blue, int level)
 	{
 		super(worldIn, xCoordIn, yCoordIn, zCoordIn, scale, 0, 0, 0);
-		this.particleMaxAge = 15;
+		this.particleMaxAge = 40;
 		this.particleRed = red;
 		this.particleGreen = green;
 		this.particleBlue = blue;
-		this.setParticleTexture(RegistrationHandler.oreParticles.get(type));
+		this.level = level;
+
+		this.setParticleTexture(Minecraft.getMinecraft().getTextureMapBlocks().getAtlasSprite(TEXTURES[0].toString()));
 	}
 
 	@Override
@@ -36,15 +52,14 @@ public class ParticleOre extends ParticleRedstone {
 	@Override
 	public void onUpdate()
 	{
+		prevTextureIndex = textureIndex;
 
 		this.prevPosX = this.posX;
 		this.prevPosY = this.posY;
 		this.prevPosZ = this.posZ;
 
 		if (this.particleAge++ >= this.particleMaxAge)
-		{
 			this.setExpired();
-		}
 
 		this.move(this.motionX, this.motionY, this.motionZ);
 
@@ -62,6 +77,19 @@ public class ParticleOre extends ParticleRedstone {
 		{
 			this.motionX *= 0.699999988079071D;
 			this.motionZ *= 0.699999988079071D;
+		}
+
+		int tickStep = (particleMaxAge / 10);
+		if (particleAge % tickStep == 0)
+		{
+
+			textureIndex++;
+
+			if (textureIndex > Math.min(level * (9 / 4), 9))
+				textureIndex = 0;
+
+			if (prevTextureIndex != textureIndex)
+				setParticleTexture(Minecraft.getMinecraft().getTextureMapBlocks().getAtlasSprite(TEXTURES[textureIndex].toString()));
 		}
 	}
 
