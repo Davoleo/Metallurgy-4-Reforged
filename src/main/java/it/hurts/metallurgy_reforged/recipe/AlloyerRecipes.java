d5ac3b0376364b74ae44bd13b8f416fa4f573b6b
Davoleo <davoleo2001@gmail.com>
2020-08-15 20:32:40 +0200

    more flexible Alloy Recipes system

--- src/main/java/it/hurts/metallurgy_reforged/recipe/AlloyerRecipes.java
+++ src/main/java/it/hurts/metallurgy_reforged/recipe/AlloyerRecipes.java
@@ -14,22 +14,21 @@ package it.hurts.metallurgy_reforged.recipe;
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
 import com.google.common.collect.Table.Cell;
+import it.hurts.metallurgy_reforged.material.Metal;
 import it.hurts.metallurgy_reforged.material.ModMetals;
+import it.hurts.metallurgy_reforged.model.AlloySample;
+import it.hurts.metallurgy_reforged.util.ItemUtils;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.oredict.OreDictionary;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class AlloyerRecipes {
 
 	private static final AlloyerRecipes INSTANCE = new AlloyerRecipes();
-	private final Table<ItemStack, ItemStack, ItemStack> alloyingList = HashBasedTable.create();
-
-	private final Map<ItemStack, Float> experienceList = new HashMap<>();
-	private final Map<ItemStack, ItemStack[]> recipeQuants = new HashMap<>();
-
+	private final Table<AlloySample, AlloySample, AlloySample> alloyingList = HashBasedTable.create();
 
 	public static AlloyerRecipes getInstance()
 	{
@@ -39,145 +38,145 @@ public class AlloyerRecipes {
 	private AlloyerRecipes()
 	{
 		this.addAlloyRecipe
-				(new ItemStack(ModMetals.COPPER.getIngot(), 3),
-						new ItemStack(ModMetals.TIN.getIngot()),
-						new ItemStack(ModMetals.BRONZE.getIngot(), 4), 1.75F);
+				(new AlloySample(ModMetals.COPPER, 3),
+						new AlloySample(ModMetals.TIN, 1),
+						new AlloySample(ModMetals.BRONZE, 4, 1.75F));
 		this.addAlloyRecipe
-				(new ItemStack(ModMetals.SHADOW_IRON.getIngot(), 2),
-						new ItemStack(ModMetals.LEMURITE.getIngot()),
-						new ItemStack(ModMetals.SHADOW_STEEL.getIngot(), 3), 1.5F);
+				(new AlloySample(ModMetals.SHADOW_IRON, 2),
+						new AlloySample(ModMetals.LEMURITE, 1),
+						new AlloySample(ModMetals.SHADOW_STEEL, 3, 1.5F));
 		this.addAlloyRecipe
-				(new ItemStack(ModMetals.ALDUORITE.getIngot()),
-						new ItemStack(ModMetals.CERUCLASE.getIngot()),
-						new ItemStack(ModMetals.INOLASHITE.getIngot(), 2), 1.25F);
+				(new AlloySample(ModMetals.ALDUORITE, 1),
+						new AlloySample(ModMetals.CERUCLASE, 1),
+						new AlloySample(ModMetals.INOLASHITE, 2, 1.25F));
 		this.addAlloyRecipe
-				(new ItemStack(ModMetals.COPPER.getIngot(), 3),
-						new ItemStack(ModMetals.ZINC.getIngot()),
-						new ItemStack(ModMetals.BRASS.getIngot(), 4), 1.75F);
+				(new AlloySample(ModMetals.COPPER, 3),
+						new AlloySample(ModMetals.ZINC, 1),
+						new AlloySample(ModMetals.BRASS, 4, 1.75F));
 		this.addAlloyRecipe
-				(new ItemStack(ModMetals.DEEP_IRON.getIngot(), 3),
-						new ItemStack(ModMetals.INFUSCOLIUM.getIngot()),
-						new ItemStack(ModMetals.BLACK_STEEL.getIngot(), 4), 1.75F);
+				(new AlloySample(ModMetals.DEEP_IRON, 3),
+						new AlloySample(ModMetals.INFUSCOLIUM, 1),
+						new AlloySample(ModMetals.BLACK_STEEL, 4, 1.75F));
 		this.addAlloyRecipe
-				(new ItemStack(Items.IRON_INGOT),
-						new ItemStack(ModMetals.MANGANESE.getIngot(), 3),
-						new ItemStack(ModMetals.STEEL.getIngot(), 2), 1.5F);
+				(new AlloySample(new ItemStack(Items.IRON_INGOT), 1),
+						new AlloySample(ModMetals.MANGANESE, 3),
+						new AlloySample(ModMetals.STEEL, 2, 1.5F));
 		this.addAlloyRecipe
-				(new ItemStack(Items.IRON_INGOT),
-						new ItemStack(ModMetals.BRONZE.getIngot(), 2),
-						new ItemStack(ModMetals.DAMASCUS_STEEL.getIngot(), 3), 1.5F);
+				(new AlloySample(new ItemStack(Items.IRON_INGOT), 1),
+						new AlloySample(ModMetals.BRONZE, 2),
+						new AlloySample(ModMetals.DAMASCUS_STEEL, 3, 1.5F));
 		this.addAlloyRecipe
-				(new ItemStack(ModMetals.SILVER.getIngot()),
-						new ItemStack(Items.GOLD_INGOT),
-						new ItemStack(ModMetals.ELECTRUM.getIngot(), 2), 1.25F);
+				(new AlloySample(ModMetals.SILVER, 1),
+						new AlloySample(new ItemStack(Items.GOLD_INGOT), 1),
+						new AlloySample(ModMetals.ELECTRUM, 2, 1.25F));
 		this.addAlloyRecipe
-				(new ItemStack(ModMetals.ORICHALCUM.getIngot()),
-						new ItemStack(ModMetals.PLATINUM.getIngot()),
-						new ItemStack(ModMetals.CELENEGIL.getIngot(), 2), 1.25F);
+				(new AlloySample(ModMetals.ORICHALCUM, 1),
+						new AlloySample(ModMetals.PLATINUM, 1),
+						new AlloySample(ModMetals.CELENEGIL, 2, 1.25F));
 		this.addAlloyRecipe
-				(new ItemStack(ModMetals.KALENDRITE.getIngot()),
-						new ItemStack(ModMetals.PLATINUM.getIngot()),
-						new ItemStack(ModMetals.AMORDRINE.getIngot(), 2), 1.25F);
+				(new AlloySample(ModMetals.KALENDRITE, 1),
+						new AlloySample(ModMetals.PLATINUM, 1),
+						new AlloySample(ModMetals.AMORDRINE, 2, 1.25F));
 		this.addAlloyRecipe(
-				new ItemStack(ModMetals.MITHRIL.getIngot()),
-				new ItemStack(ModMetals.RUBRACIUM.getIngot(), 2),
-				new ItemStack(ModMetals.HADEROTH.getIngot(), 3), 1.5F);
+				new AlloySample(ModMetals.MITHRIL, 1),
+				new AlloySample(ModMetals.RUBRACIUM, 2),
+				new AlloySample(ModMetals.HADEROTH, 3, 1.5F));
 		this.addAlloyRecipe
-				(new ItemStack(ModMetals.ADAMANTINE.getIngot()),
-						new ItemStack(ModMetals.ATLARUS.getIngot()),
-						new ItemStack(ModMetals.TARTARITE.getIngot()), 1.5F); //output decreased by one for balance sake
+				(new AlloySample(ModMetals.ADAMANTINE, 1),
+						new AlloySample(ModMetals.ATLARUS, 1),
+						new AlloySample(ModMetals.TARTARITE, 1, 1.5F)); //output decreased by one for balance sake
 		this.addAlloyRecipe
-				(new ItemStack(ModMetals.EXIMITE.getIngot()),
-						new ItemStack(ModMetals.MEUTOITE.getIngot()),
-						new ItemStack(ModMetals.DESICHALKOS.getIngot(), 2), 1.25F);
+				(new AlloySample(ModMetals.EXIMITE, 1),
+						new AlloySample(ModMetals.MEUTOITE, 1),
+						new AlloySample(ModMetals.DESICHALKOS, 2, 1.25F));
 		this.addAlloyRecipe
-				(new ItemStack(Items.IRON_INGOT),
-						new ItemStack(Items.GOLD_INGOT),
-						new ItemStack(ModMetals.ANGMALLEN.getIngot(), 2), 1.25F);
+				(new AlloySample(new ItemStack(Items.GOLD_INGOT), 1),
+						new AlloySample(new ItemStack(Items.IRON_INGOT), 1),
+						new AlloySample(ModMetals.ANGMALLEN, 2, 1.25F));
 		//      Original Recipe: [Bronze + Gold] 1:1 = Hepatizon
 		this.addAlloyRecipe
-				(new ItemStack(ModMetals.INFUSCOLIUM.getIngot()),
-						new ItemStack(ModMetals.STEEL.getIngot()),
-						new ItemStack(ModMetals.HEPATIZON.getIngot(), 2), 1.25F);
+				(new AlloySample(ModMetals.INFUSCOLIUM, 1),
+						new AlloySample(ModMetals.STEEL, 1),
+						new AlloySample(ModMetals.HEPATIZON, 2, 1.25F));
 		this.addAlloyRecipe
-				(new ItemStack(ModMetals.SILVER.getIngot()),
-						new ItemStack(ModMetals.MITHRIL.getIngot()),
-						new ItemStack(ModMetals.QUICKSILVER.getIngot(), 2), 1.25F);
+				(new AlloySample(ModMetals.SILVER, 1),
+						new AlloySample(ModMetals.MITHRIL, 1),
+						new AlloySample(ModMetals.QUICKSILVER, 2, 1.25F));
 		this.addAlloyRecipe
-				(new ItemStack(ModMetals.LUTETIUM.getIngot()),
-						new ItemStack(ModMetals.OSMIUM.getIngot()),
-						new ItemStack(ModMetals.KRIK.getIngot(), 2), 1.25F);
+				(new AlloySample(ModMetals.LUTETIUM, 1),
+						new AlloySample(ModMetals.OSMIUM, 1),
+						new AlloySample(ModMetals.KRIK, 2, 1.25F));
 		this.addAlloyRecipe
-				(new ItemStack(ModMetals.SANGUINITE.getIngot()),
-						new ItemStack(ModMetals.CARMOT.getIngot()),
-						new ItemStack(ModMetals.ETHERIUM.getIngot(), 2), 1.25F);
+				(new AlloySample(ModMetals.SANGUINITE, 1),
+						new AlloySample(ModMetals.CARMOT, 1),
+						new AlloySample(ModMetals.ETHERIUM, 2, 1.25F));
 	}
 
-	public void addAlloyRecipe(ItemStack input1, ItemStack input2, ItemStack result, float experience)
+	public void addCustomAlloyRecipe(ItemStack input1, ItemStack input2, ItemStack result, float xp)
 	{
-
 		if (input1.isEmpty() || input2.isEmpty() || result.isEmpty())
 			return;
+		//Won't add a recipe whose inputs already correspond to an output in existing recipes
 		if (getAlloyResult(input1, input2) != ItemStack.EMPTY)
 			return;
 
-		this.alloyingList.put(input1, input2, result);
-		this.experienceList.put(result, experience);
-		this.recipeQuants.put(result, new ItemStack[]{input1, input2});
+		this.alloyingList.put(
+				new AlloySample(input1, input1.getCount()),
+				new AlloySample(input2, input2.getCount()),
+				new AlloySample(result, result.getCount()).setXp(xp)
+		);
 	}
 
-	@SuppressWarnings("ConstantConditions")
+	public void addAlloyRecipe(AlloySample input1, AlloySample input2, AlloySample result)
+	{
+		this.alloyingList.put(input1, input2, result);
+	}
+
 	public void removeAlloyRecipe(ItemStack output)
 	{
-		ItemStack firstInput = ItemStack.EMPTY;
-		ItemStack secondInput = ItemStack.EMPTY;
+		Set<Cell<AlloySample, AlloySample, AlloySample>> recipesToRemove = this.alloyingList.cellSet().stream()
+				.filter(cell -> compareItemStackToAlloySample(output, cell.getValue()))
+				.collect(Collectors.toSet());
 
-		for (Table.Cell<ItemStack, ItemStack, ItemStack> recipe : getInstance().getRecipeTable().cellSet())
-		{
-			if (ItemStack.areItemStacksEqual(output, recipe.getValue()))
-			{
-				firstInput = recipe.getRowKey();
-				secondInput = recipe.getColumnKey();
-			}
-		}
-
-		if (!firstInput.isEmpty() && !secondInput.isEmpty())
-		{
-			alloyingList.remove(firstInput, secondInput);
-			experienceList.remove(output);
-			recipeQuants.remove(output);
-		}
+		if (!recipesToRemove.isEmpty())
+			recipesToRemove.forEach(alloyCell -> alloyingList.remove(alloyCell.getRowKey(), alloyCell.getColumnKey()));
 	}
 
 	@SuppressWarnings("ConstantConditions")
 	public ItemStack getAlloyResult(ItemStack input1, ItemStack input2)
 	{
-		for (Cell<ItemStack, ItemStack, ItemStack> cell : this.alloyingList.cellSet())
-		{
-			if (this.compareItemStacks(cell.getColumnKey(), input1) && this.compareItemStacks(cell.getRowKey(), input2))
-			{
-				return cell.getValue();
-			}
+		Optional<Cell<AlloySample, AlloySample, AlloySample>> result =
+				this.alloyingList.cellSet().stream()
+						.filter(cell ->
+								(compareItemStackToAlloySample(input1, cell.getRowKey()) && compareItemStackToAlloySample(input2, cell.getColumnKey()))
+										|| (compareItemStackToAlloySample(input1, cell.getColumnKey()) && compareItemStackToAlloySample(input2, cell.getRowKey()))
+						).findFirst();
 
-			if (this.compareItemStacks(cell.getRowKey(), input1) && this.compareItemStacks(cell.getColumnKey(), input2))
-			{
-				return cell.getValue();
-			}
+		//check if there's any recipe that has those inputs (when the optional is not empty)
+		if (result.isPresent())
+		{
+			//get the alloy sample object
+			AlloySample alloyResult = result.get().getValue();
+			return alloyResult.getStack();
 		}
-		return ItemStack.EMPTY;
+		else
+			return ItemStack.EMPTY;
 	}
 
-	@SuppressWarnings("ConstantConditions")
 	public boolean isAlloyMetal(ItemStack input1)
 	{
-		for (Cell<ItemStack, ItemStack, ItemStack> cell : this.alloyingList.cellSet())
-		{
-			if (this.compareItemStacks(cell.getColumnKey(), input1) || this.compareItemStacks(cell.getRowKey(), input1))
-			{
-				return true;
-			}
-		}
-		return false;
+		return this.alloyingList.cellSet().stream()
+				.anyMatch(cell -> compareItemStackToAlloySample(input1, cell.getRowKey()) || compareItemStackToAlloySample(input1, cell.getColumnKey()));
+	}
+
+	private boolean compareItemStackToAlloySample(ItemStack stack, AlloySample alloySample)
+	{
+		Metal metal = ItemUtils.getMetalFromOreDictStack(stack);
+
+		if (metal != null)
+			return metal == alloySample.getMetal();
+		else
+			return compareItemStacks(stack, alloySample.getFallbackStack());
 	}
 
 	private boolean compareItemStacks(ItemStack stack1, ItemStack stack2)
@@ -187,28 +186,38 @@ public class AlloyerRecipes {
 
 	public float getAlloyExperience(ItemStack stack)
 	{
-		for (ItemStack in : this.experienceList.keySet())
-		{
-			if (in.isItemEqual(stack) || (in.getItem() == stack.getItem() && in.getItemDamage() == OreDictionary.WILDCARD_VALUE))
-				return experienceList.get(in);
-		}
-		return 0.0F;
+		Optional<AlloySample> result = this.alloyingList.cellSet().stream()
+				.map(Cell::getValue)
+				.filter(resultAlloy -> compareItemStackToAlloySample(stack, resultAlloy))
+				.findFirst();
+
+		return result
+				.filter(AlloySample::hasXp)
+				.map(AlloySample::getXp)
+				.orElse(0F);
 	}
 
 	public int getItemQuantity(ItemStack result, ItemStack input)
 	{
-		if (this.recipeQuants.containsKey(result))
+		for (Cell<AlloySample, AlloySample, AlloySample> cell : this.alloyingList.cellSet())
 		{
-			for (ItemStack stack : this.recipeQuants.get(result))
+			if (compareItemStackToAlloySample(result, cell.getValue()))
 			{
-				if (this.compareItemStacks(stack, input))
-					return stack.getCount();
+
+				if (compareItemStackToAlloySample(input, cell.getRowKey()))
+				{
+					return cell.getRowKey().getAmount();
+				}
+				else if (compareItemStackToAlloySample(input, cell.getColumnKey()))
+				{
+					return cell.getColumnKey().getAmount();
+				}
 			}
 		}
 		return 0;
 	}
 
-	public Table<ItemStack, ItemStack, ItemStack> getRecipeTable()
+	public Table<AlloySample, AlloySample, AlloySample> getRecipeTable()
 	{
 		return alloyingList;
 	}
