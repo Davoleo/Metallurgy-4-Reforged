03401d25a64b5818c83de01914891b09a8a8f52b
Davoleo <davoleo2001@gmail.com>
2020-09-01 21:03:02 +0200

    Fix #217 | NetherEX Integration

--- src/main/java/it/hurts/metallurgy_reforged/world/spawn/IOreSpawn.java
+++ src/main/java/it/hurts/metallurgy_reforged/world/spawn/IOreSpawn.java
@@ -1,10 +1,20 @@
+/*
+ * -------------------------------------------------------------------------------------------------------
+ * Class: IOreSpawn
+ * This class is part of Metallurgy 4 Reforged
+ * Complete source code is available at: https://github.com/Davoleo/Metallurgy-4-Reforged
+ * This code is licensed under GNU GPLv3
+ * Authors: Davoleo, ItHurtsLikeHell, PierKnight100
+ * Copyright (c) 2020.
+ * --------------------------------------------------------------------------------------------------------
+ */
+
 package it.hurts.metallurgy_reforged.world.spawn;
 
+import com.google.common.base.Predicate;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-
-import com.google.common.base.Predicate;
 
 import java.util.Random;
 
@@ -20,9 +30,9 @@ public interface IOreSpawn
     boolean canOreSpawn(World world, BlockPos pos, IBlockState state, Random random);
 
     /**
-     *
      * @return the predicate that establish if the ore can spawn by replacing the previews block
      */
+    @SuppressWarnings("Guava")
     Predicate<IBlockState> getBlockPredicate();
 
     /**
--- src/main/java/it/hurts/metallurgy_reforged/world/spawn/SmallIslandsSpawn.java
+++ src/main/java/it/hurts/metallurgy_reforged/world/spawn/SmallIslandsSpawn.java
@@ -1,3 +1,14 @@
+/*
+ * -------------------------------------------------------------------------------------------------------
+ * Class: SmallIslandsSpawn
+ * This class is part of Metallurgy 4 Reforged
+ * Complete source code is available at: https://github.com/Davoleo/Metallurgy-4-Reforged
+ * This code is licensed under GNU GPLv3
+ * Authors: Davoleo, ItHurtsLikeHell, PierKnight100
+ * Copyright (c) 2020.
+ * --------------------------------------------------------------------------------------------------------
+ */
+
 package it.hurts.metallurgy_reforged.world.spawn;
 
 import it.hurts.metallurgy_reforged.world.ModWorldGen;
@@ -12,7 +23,7 @@ public class SmallIslandsSpawn extends BaseOreSpawn
 {
     public SmallIslandsSpawn(Biome[] biomes)
     {
-        super(ModWorldGen.DEFAULT_END,biomes);
+        super(ModWorldGen.DEFAULT_END_BLOCK, biomes);
     }
 
     @Override
