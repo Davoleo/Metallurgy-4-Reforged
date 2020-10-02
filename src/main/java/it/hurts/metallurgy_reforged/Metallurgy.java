98b6cdb1b5fb7cb1cdbdfe22af21973b87fd034b
Davoleo <davoleo2001@gmail.com>
2020-10-02 19:51:44 +0200

    Bump Mod Version to 1.1.1
    + Update Changelog.txt

--- src/main/java/it/hurts/metallurgy_reforged/Metallurgy.java
+++ src/main/java/it/hurts/metallurgy_reforged/Metallurgy.java
@@ -57,7 +57,7 @@ public class Metallurgy {
 
 	public static final String MODID = "metallurgy";
 	public static final String NAME = "Metallurgy 4: Reforged";
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.1.1";
 
 	public static Logger logger;
 
