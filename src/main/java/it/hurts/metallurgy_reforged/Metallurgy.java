61d79cf57dddbeabdfaf169cfb7c1926a4ce3e0e
Davoleo <davoleo2001@gmail.com>
2020-11-07 00:56:11 +0100

    Bump version to 1.2.0-alpha.1

--- src/main/java/it/hurts/metallurgy_reforged/Metallurgy.java
+++ src/main/java/it/hurts/metallurgy_reforged/Metallurgy.java
@@ -58,7 +58,7 @@ public class Metallurgy {
 
 	public static final String MODID = "metallurgy";
 	public static final String NAME = "Metallurgy 4: Reforged";
-	public static final String VERSION = "1.1.2";
+	public static final String VERSION = "1.2.0-alpha.1";
 
 	public static Logger logger;
 
