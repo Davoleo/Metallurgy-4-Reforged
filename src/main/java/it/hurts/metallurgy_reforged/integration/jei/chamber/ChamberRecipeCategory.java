8d1856686a3424506df815233fe3e5656ee5d8d6
Davoleo <davoleo2001@gmail.com>
2020-11-07 10:45:30 +0100

    Update Copyright headers

--- src/main/java/it/hurts/metallurgy_reforged/integration/jei/chamber/ChamberRecipeCategory.java
+++ src/main/java/it/hurts/metallurgy_reforged/integration/jei/chamber/ChamberRecipeCategory.java
@@ -1,13 +1,11 @@
-/*
- * -------------------------------------------------------------------------------------------------------
- * Class: ChamberRecipeCategory
- * This class is part of Metallurgy 4 Reforged
- * Complete source code is available at: https://github.com/Davoleo/Metallurgy-4-Reforged
- * This code is licensed under GNU GPLv3
- * Authors: Davoleo, ItHurtsLikeHell, PierKnight100
- * Copyright (c) 2020.
- * --------------------------------------------------------------------------------------------------------
- */
+/*==============================================================================
+ = Class: ChamberRecipeCategory
+ = This class is part of Metallurgy 4: Reforged
+ = Complete source code is available at https://github.com/Davoleo/Metallurgy-4-Reforged
+ = This code is licensed under GNU GPLv3
+ = Authors: Davoleo, ItHurtsLikeHell, PierKnight100
+ = Copyright (c) 2018-2020.
+ =============================================================================*/
 
 package it.hurts.metallurgy_reforged.integration.jei.chamber;
 
--- src/main/java/it/hurts/metallurgy_reforged/render/BrassKnucklesBakedModel.java
+++ src/main/java/it/hurts/metallurgy_reforged/render/BrassKnucklesBakedModel.java
@@ -1,3 +1,12 @@
+/*==============================================================================
+ = Class: BrassKnucklesBakedModel
+ = This class is part of Metallurgy 4: Reforged
+ = Complete source code is available at https://github.com/Davoleo/Metallurgy-4-Reforged
+ = This code is licensed under GNU GPLv3
+ = Authors: Davoleo, ItHurtsLikeHell, PierKnight100
+ = Copyright (c) 2018-2020.
+ =============================================================================*/
+
 package it.hurts.metallurgy_reforged.render;
 
 import it.hurts.metallurgy_reforged.Metallurgy;
