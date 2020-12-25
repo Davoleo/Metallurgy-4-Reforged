d3fcdd07162ca257787ad0f6e874ca1f0c878f14
PierKnight100 <pier.altimari@libero.it>
2020-12-25 11:28:43 +0100

    Adamantine Armor Effect - Now It works with mobs too - fixed regeneration added when the players dies in general with or without armor

--- src/main/java/it/hurts/metallurgy_reforged/effect/armor/AdamantineArmorEffect.java
+++ src/main/java/it/hurts/metallurgy_reforged/effect/armor/AdamantineArmorEffect.java
@@ -56,13 +56,15 @@ public class AdamantineArmorEffect extends BaseMetallurgyEffect {
         if (!canBeApplied(entity))
             return;
 
-        if (!entity.world.isRemote && entity instanceof EntityPlayerMP) {
+        if (!entity.world.isRemote)
+        {
             //Get a random slot from the Armor slots (the "2 +" skips main hand and off hand slots)
             EntityEquipmentSlot randomArmorSlot = EntityEquipmentSlot.values()[2 + Utils.random.nextInt(4)];
 
             ItemStack armorPiece = entity.getItemStackFromSlot(randomArmorSlot);
             //Check whether the itemStack inside the random slot is an Adamantine armor piece
-            if (ItemUtils.isMadeOfMetal(metal, armorPiece.getItem())) {
+            if (ItemUtils.isMadeOfMetal(metal, armorPiece.getItem()))
+            {
                 //Remove armor piece from the slot
                 entity.setItemStackToSlot(randomArmorSlot, ItemStack.EMPTY);
                 //Play Totem of Undying sound
@@ -74,19 +76,23 @@ public class AdamantineArmorEffect extends BaseMetallurgyEffect {
                 //Cancel Player Death
                 event.setCanceled(true);
 
-                //Send a packet to emit particles and render the Totem item overlay
-                PacketRenderDeathProtection packet = new PacketRenderDeathProtection(entity, armorPiece);
-                //This criteria needs to be triggered in order to the totem overlay to work
-                CriteriaTriggers.USED_TOTEM.trigger(((EntityPlayerMP) entity), armorPiece);
+                if (entity instanceof EntityPlayerMP)
+                {
+                    //Send a packet to emit particles and render the Totem item overlay
+                    PacketRenderDeathProtection packet = new PacketRenderDeathProtection(entity, armorPiece);
+                    //This criteria needs to be triggered in order to the totem overlay to work
+                    CriteriaTriggers.USED_TOTEM.trigger(((EntityPlayerMP) entity), armorPiece);
 
-                //Send two packets to the client (one to self and many to the other players that are watching the entity)
-                PacketManager.network.sendTo(packet, ((EntityPlayerMP) entity));
-                PacketManager.network.sendToAllTracking(packet, entity);
+                    //Send two packets to the client (one to self and many to the other players that are watching the entity)
+                    PacketManager.network.sendTo(packet, ((EntityPlayerMP) entity));
+                    PacketManager.network.sendToAllTracking(packet, entity);
+                }
+
+                //Give absorption and regeneration III to the player so that they don't die instantly after being revived
+                entity.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 20 * 25, 2));
+                entity.addPotionEffect(new PotionEffect(MobEffects.ABSORPTION, 60, 1));
             }
         }
 
-        //Give absorption and regeneration III to the player so that they don't die instantly after being revived
-        entity.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 20 * 25, 2));
-        entity.addPotionEffect(new PotionEffect(MobEffects.ABSORPTION, 60, 1));
     }
 }
