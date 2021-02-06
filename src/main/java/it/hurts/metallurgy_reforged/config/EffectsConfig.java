b4f69cae5df1e176ee4912422e359c00551ddcfc
Davoleo <davoleo2001@gmail.com>
2021-02-06 21:16:07 +0100

    aquatic missing tooltips

--- src/main/java/it/hurts/metallurgy_reforged/config/EffectsConfig.java
+++ src/main/java/it/hurts/metallurgy_reforged/config/EffectsConfig.java
@@ -111,6 +111,10 @@ public class EffectsConfig {
     @Config.Comment("Mining ores sometimes grants a bit of experience depending on the harvest level.")
     public static boolean damascusSteelEffectPickaxe = true;
 
+    @Config.Name("Aquatic")
+    @Config.Comment("Grants Water Breathing and improved Mobility in water depending on how many armor pieces are worn (it also grants night vision with 2 or more armor pieces).")
+    public static boolean deepIronEffectArmor = true;
+
     @Config.Name("Weight-Controlled Flight")
     @Config.Comment("Makes you float at a certain height depending on how much filled your inventory is. You can press the UP arrow or the DOWN arrow to float respectively up and down.  You have a height limit you can't go over while floating, that limit depends on how full your inventory is (excluding the hotbar). There's also an HUD in the bottom-right corner of the screen that shows you, your height level and your limit.")
     public static boolean krikEffectArmor = true;
