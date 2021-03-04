537124cf321a5279ddea0212dea0756c55ea595d
Davoleo <davoleo2001@gmail.com>
2021-03-04 13:52:47 +0100

    build(patch): Bump to 1.2.5

--- src/main/java/it/hurts/metallurgy_reforged/Metallurgy.java
+++ src/main/java/it/hurts/metallurgy_reforged/Metallurgy.java
@@ -56,7 +56,7 @@ public class Metallurgy {
 
 	public static final String MODID = "metallurgy";
 	public static final String NAME = "Metallurgy 4: Reforged";
-	public static final String VERSION = "@VERSION@";
+	public static final String VERSION = "1.2.5";
 
 	public static Logger logger;
 
