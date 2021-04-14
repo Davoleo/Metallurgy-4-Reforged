0261ddf9573dd46095938030d9a261af0f98a695
Davoleo <davoleo2001@gmail.com>
2021-04-14 16:54:02 +0200

    revert: "Fixed Biome Ore generation Config"
    This reverts commit 77c25eb4

--- src/main/java/it/hurts/metallurgy_reforged/world/spawn/BaseOreSpawn.java
+++ src/main/java/it/hurts/metallurgy_reforged/world/spawn/BaseOreSpawn.java
@@ -4,7 +4,7 @@
  = Complete source code is available at https://github.com/Davoleo/Metallurgy-4-Reforged
  = This code is licensed under GNU GPLv3
  = Authors: Davoleo, ItHurtsLikeHell, PierKnight100
- = Copyright (c) 2018-2020.
+ = Copyright (c) 2018-2021.
  =============================================================================*/
 
 package it.hurts.metallurgy_reforged.world.spawn;
@@ -54,7 +54,7 @@ public class BaseOreSpawn implements IOreSpawn {
                 if (world.getBiome(pos) == biome)
                     return true;
         }
-        return false;
+        return true;
     }
 
 }
