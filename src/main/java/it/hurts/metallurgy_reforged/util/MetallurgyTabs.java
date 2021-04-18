bac906615c6dc127947a0ac5f579e040f571b8a9
Davoleo <davoleo2001@gmail.com>
2021-04-18 17:34:02 +0200

    feat: better creative tab handling when content is disabled via config

--- src/main/java/it/hurts/metallurgy_reforged/util/MetallurgyTabs.java
+++ src/main/java/it/hurts/metallurgy_reforged/util/MetallurgyTabs.java
@@ -12,6 +12,8 @@ package it.hurts.metallurgy_reforged.util;
 import it.hurts.metallurgy_reforged.Metallurgy;
 import it.hurts.metallurgy_reforged.block.BlockTypes;
 import it.hurts.metallurgy_reforged.block.ModBlocks;
+import it.hurts.metallurgy_reforged.config.RegistrationConfig;
+import it.hurts.metallurgy_reforged.item.ModItems;
 import it.hurts.metallurgy_reforged.item.tool.EnumTools;
 import it.hurts.metallurgy_reforged.material.Metal;
 import it.hurts.metallurgy_reforged.material.ModMetals;
@@ -25,25 +27,34 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nonnull;
+import java.util.Arrays;
 import java.util.Objects;
+import java.util.Optional;
 
+@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
 public class MetallurgyTabs extends CreativeTabs {
 
 	//TODO : Fix Creative tab icon ticking
 
 	public static final CreativeTabs tabArmor, tabBlock, tabDust, tabFluid, tabIngot, tabNugget, tabSpecial, tabOre, tabTool;
 
+	private static final Optional<BlockTypes> firstEnabledBlockType;
+	private static final Optional<EnumTools> firstEnabledToolType;
+
 	static
 	{
-		tabArmor = new MetallurgyTabs(0, "armors");
-		tabBlock = new MetallurgyTabs(1, "blocks");
+		firstEnabledBlockType = Arrays.stream(BlockTypes.values()).filter(BlockTypes::isEnabled).findFirst();
+		firstEnabledToolType = Arrays.stream(EnumTools.values()).filter(EnumTools::isEnabled).findFirst();
+
+		tabArmor = RegistrationConfig.categoryItems.enableMetalArmorSets ? new MetallurgyTabs(0, "armors") : null;
+		tabBlock = firstEnabledBlockType.isPresent() ? new MetallurgyTabs(1, "blocks") : null;
 		tabDust = new MetallurgyTabs(2, "dusts");
 		tabFluid = new MetallurgyTabs(3, "fluids");
 		tabIngot = new MetallurgyTabs(4, "ingots");
-		tabNugget = new MetallurgyTabs(5, "nuggets");
+		tabNugget = RegistrationConfig.categoryItems.enableMetalNuggets ? new MetallurgyTabs(5, "nuggets") : null;
 		tabSpecial = new MetallurgyTabs(6, "special");
 		tabOre = new MetallurgyTabs(7, "ores");
-		tabTool = new MetallurgyTabs(8, "tools");
+		tabTool = firstEnabledToolType.isPresent() ? new MetallurgyTabs(8, "tools") : null;
 	}
 
 	private final int type;
@@ -59,6 +70,8 @@ public class MetallurgyTabs extends CreativeTabs {
 		return Metallurgy.MODID + "." + name;
 	}
 
+	//Suppressing isPresent check because the method will never be called if the creative tab is not initialized above
+	@SuppressWarnings("OptionalGetWithoutIsPresent")
 	@SideOnly(Side.CLIENT)
 	@Nonnull
 	@Override
@@ -68,29 +81,29 @@ public class MetallurgyTabs extends CreativeTabs {
 		if (type == 6)
 			return new ItemStack(ModBlocks.crusher);
 
-		Metal adamantine = ModMetals.ADAMANTINE;
+		Optional<Metal> firstAvailableMetal = ModMetals.metalMap.values().stream().findFirst();
 
-		if (adamantine == null)
+		if (!firstAvailableMetal.isPresent())
 			return ItemStack.EMPTY;
 
 		switch (type)
 		{
 			case 0:
-				return new ItemStack(adamantine.getArmorPiece(EntityEquipmentSlot.CHEST));
+				return new ItemStack(firstAvailableMetal.get().getArmorPiece(EntityEquipmentSlot.CHEST));
 			case 1:
-				return new ItemStack(adamantine.getBlock(BlockTypes.BLOCK));
+				return new ItemStack(firstAvailableMetal.get().getBlock(firstEnabledBlockType.get()));
 			case 2:
-				return new ItemStack(adamantine.getDust());
+				return new ItemStack(RegistrationConfig.categoryItems.enableMetalDusts ? firstAvailableMetal.get().getDust() : ModItems.phosphorus);
 			case 3:
-				return FluidUtil.getFilledBucket(ModMetals.ADAMANTINE.getMolten().getFluidStack());
+				return FluidUtil.getFilledBucket(firstAvailableMetal.get().getMolten().getFluidStack());
 			case 4:
-				return new ItemStack(adamantine.getIngot());
+				return new ItemStack(firstAvailableMetal.get().getIngot());
 			case 5:
-				return new ItemStack(adamantine.getNugget());
+				return new ItemStack(firstAvailableMetal.get().getNugget());
 			case 7:
-				return new ItemStack(Objects.requireNonNull(adamantine.getOre()));
+				return ModMetals.ADAMANTINE != null ? new ItemStack(Objects.requireNonNull(ModMetals.ADAMANTINE.getOre())) : ItemStack.EMPTY;
 			case 8:
-				return new ItemStack(adamantine.getTool(EnumTools.PICKAXE));
+				return new ItemStack(firstAvailableMetal.get().getTool(firstEnabledToolType.get()));
 			default:
 				return ItemStack.EMPTY;
 		}
