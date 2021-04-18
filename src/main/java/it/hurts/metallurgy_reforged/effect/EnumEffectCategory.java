1ca5b15f59a4e76d0fc5bcc9736a1e3179ab566a
Davoleo <davoleo2001@gmail.com>
2021-04-18 18:46:20 +0200

    fix: some merge mistakes

--- src/main/java/it/hurts/metallurgy_reforged/effect/EnumEffectCategory.java
+++ src/main/java/it/hurts/metallurgy_reforged/effect/EnumEffectCategory.java
@@ -4,12 +4,12 @@
  = Complete source code is available at https://github.com/Davoleo/Metallurgy-4-Reforged
  = This code is licensed under GNU GPLv3
  = Authors: Davoleo, ItHurtsLikeHell, PierKnight100
- = Copyright (c) 2018-2020.
+ = Copyright (c) 2018-2021.
  =============================================================================*/
 
 package it.hurts.metallurgy_reforged.effect;
 
-import it.hurts.metallurgy_reforged.model.EnumTools;
+import it.hurts.metallurgy_reforged.item.tool.EnumTools;
 
 public enum EnumEffectCategory {
     TOOL(EnumTools.AXE, EnumTools.PICKAXE, EnumTools.SHOVEL),
