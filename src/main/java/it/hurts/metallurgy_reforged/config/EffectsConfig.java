b5237d79aa94117a93bc1c5c487690601612084a
Klazkin <matvei.matpac@gmail.com>
2021-05-19 16:41:38 +0300

    Krik effect description fix

--- src/main/java/it/hurts/metallurgy_reforged/config/EffectsConfig.java
+++ src/main/java/it/hurts/metallurgy_reforged/config/EffectsConfig.java
@@ -205,7 +205,7 @@ public class EffectsConfig {
     @Config.Comment("Makes you float at a certain height depending on how much filled your inventory is. You can press the UP arrow or the DOWN arrow to float respectively up and down.  You have a height limit you can't go over while floating, that limit depends on how full your inventory is (excluding the hotbar). There's also an HUD in the bottom-right corner of the screen that shows you, your height level and your limit.")
     public static boolean krikEffectArmor = true;
     @Config.Name("Krik-Drives")
-    @Config.Comment("Activating the item launches the player in the general direction of where they are looking and deals weapon damage to mobs that were passed.")
+    @Config.Comment("Activating the item launches the player in the general direction of where they are looking.")
     public static boolean krikEffectTool = true;
 
     private EffectsConfig()
