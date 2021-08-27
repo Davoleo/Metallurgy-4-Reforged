a41c8e0ea0470479cee63fdf8139d77a4dd7e8ee
Davoleo <dav@davoleo.net>
2021-08-27 13:01:55 +0200

    effect: Tartarite Paragon - add inventory tick and right click support

--- src/main/java/it/hurts/metallurgy_reforged/effect/all/TartariteEffect.java
+++ src/main/java/it/hurts/metallurgy_reforged/effect/all/TartariteEffect.java
@@ -11,10 +11,17 @@ package it.hurts.metallurgy_reforged.effect.all;
 
 import it.hurts.metallurgy_reforged.effect.BaseMetallurgyEffect;
 import it.hurts.metallurgy_reforged.effect.EnumEffectCategory;
+import it.hurts.metallurgy_reforged.effect.MetallurgyEffects;
+import it.hurts.metallurgy_reforged.item.tool.EnumTools;
 import it.hurts.metallurgy_reforged.material.Metal;
 import it.hurts.metallurgy_reforged.material.ModMetals;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+import org.apache.commons.lang3.ArrayUtils;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -31,6 +38,46 @@ public class TartariteEffect extends BaseMetallurgyEffect {
 	public EnumEffectCategory getCategory()
 	{
 		return EnumEffectCategory.ALL;
+	}
+
+	@Override
+	public void inventoryTick(@Nonnull ItemStack stack, @Nonnull World worldIn, @Nonnull Entity entityIn, int itemSlot, boolean isSelected)
+	{
+		Metal metal = getParagonMetal(stack);
+		if (metal == null)
+			return;
+
+		EnumTools toolType = EnumTools.byInstance(stack.getItem());
+
+		if (toolType != null)
+		{
+			MetallurgyEffects.effects.row(metal).forEach((category, effect) -> {
+				if (ArrayUtils.contains(category.getTools(), toolType))
+					effect.inventoryTick(stack, worldIn, entityIn, itemSlot, isSelected);
+			});
+		}
+		else
+			MetallurgyEffects.effects.get(metal, EnumEffectCategory.ARMOR)
+					.inventoryTick(stack, worldIn, entityIn, itemSlot, isSelected);
+	}
+
+	@Override
+	public void rightClickHandler(@Nonnull World worldIn, @Nonnull EntityPlayer playerIn, @Nonnull EnumHand handIn)
+	{
+		ItemStack stack = playerIn.getHeldItem(handIn);
+
+		Metal metal = getParagonMetal(stack);
+		if (metal == null)
+			return;
+
+		EnumTools toolType = EnumTools.byInstance(stack.getItem());
+		if (toolType == null)
+			return;
+
+		MetallurgyEffects.effects.row(metal).forEach((category, effect) -> {
+			if (ArrayUtils.contains(category.getTools(), toolType))
+				effect.rightClickHandler(worldIn, playerIn, handIn);
+		});
 	}
 
 	public static void setParagon(ItemStack tartarStack, @Nonnull Metal metal)
