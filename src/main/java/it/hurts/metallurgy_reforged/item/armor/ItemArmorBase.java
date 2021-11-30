1b2f9e175002e39ee44a12810cfc3af256899a76
Davoleo <dav@davoleo.net>
2021-11-30 21:45:21 +0100

    Durability stat tooltips on armor items

--- src/main/java/it/hurts/metallurgy_reforged/item/armor/ItemArmorBase.java
+++ src/main/java/it/hurts/metallurgy_reforged/item/armor/ItemArmorBase.java
@@ -124,6 +124,7 @@ public class ItemArmorBase extends ItemArmor implements IMetalItem {
     @Override
     public void addInformation(@Nonnull ItemStack stack, @Nullable World worldIn, @Nonnull List<String> tooltip, @Nonnull ITooltipFlag flagIn)
     {
+        ItemUtils.buildStatsTooltip(tooltip, null, null, stack);
         ItemUtils.buildEffectTooltip(tooltip, effects, stack, null);
     }
 
